365c688dff663b849603157a5b7d3459555c9841
Arif Khan <arifkhan2@gmail.com>
2015-01-24 19:44:11 -0600

    Renamed files

--- WCF Library/ITestService1.cs
+++ WCF Library/ITestService1.cs
@@ -9,7 +9,7 @@ namespace WCF_Library
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface ITestService1
     {
         [OperationContract]
         string GetData(int value);
--- WCF Library/TestService1.cs
+++ WCF Library/TestService1.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace WCF_Library
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class Service1 : IService1
+    public class TestService1 : ITestService1
     {
         public string GetData(int value)
         {
