0243585cdaac9c83dc9e8b6ce521d714ce0ded4f
arifkhan2 <arifkhan2@gmail.com>
2015-02-03 17:01:02 -0600

    Added SQL Cache Example

---
+++ Chap03.Proj02.SqlCache/Cacher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using System.Collections.Specialized;
+using System.Data.SqlClient;
+
+namespace Chap03.Proj02.SqlCache
+{
+    public class Cacher
+    {
+        private MemoryCache cache = new MemoryCache("test");
+
+        public Boolean HasData
+        {
+            get { return cache.Contains("data"); }
+        }
+
+        public void Start()
+        {
+            var connectionString = @"Data Source=.;Initial Catalog=CachingTest;Integrated Security=True";
+            var list = new NameValueCollection();
+            var policy = new CacheItemPolicy();
+
+            SqlDependency.Start(connectionString);
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand("SELECT Name, Value FROM dbo.Table_1", connection))
+                {
+                    var dependency = new SqlDependency(command);
+
+                    connection.Open();
+
+                    var reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        list.Add(reader["Name"].ToString(), reader["Value"].ToString());
+                    }
+
+                    var monitor = new SqlChangeMonitor(dependency);
+
+                    policy.ChangeMonitors.Add(monitor);
+                }
+
+                cache.Add("data", list, policy);
+            }
+        }
+    }
+}
---
+++ Chap03.Proj02.SqlCache/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chap03.Proj02.SqlCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cacher = new Cacher();
+
+            cacher.Start();
+
+            Console.WriteLine("Watching for changes...");
+
+            while (true)
+            {
+                if (cacher.HasData)
+                {
+                    Console.Write(".");
+                    Thread.Sleep(1000);
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Cache EMPTY!");
+                    Console.ReadLine();
+                    cacher.Start();
+                }
+            }
+        }
+    }
+}
